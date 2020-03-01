10f92475ca7b390212bcd0a386113704ddc7c210
Garrett Thompson <garrett.leland.thompson@gmail.com>
2020-02-29 21:22:07 -0800

    cleaning up method names, formatting

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -3,29 +3,31 @@ class ConverterController < ApplicationController
     end
 
     def create
+        input_to_format = {"Decimal" => "convert_to_sexigesimal", "Sexigesimal" => "convert_to_decimal"}
         @answers = []
-        send("format_#{params[:data].downcase}")
+        send(input_to_format[params[:data]])
     end
 
-  def format_sexigesimal
+  private
+
+  def convert_to_sexigesimal
     params[:form][:body].split("\n").each do |line|
         ra, dec = line.split(" ")
         @answers << to_sexigesimal(ra, dec)
     end
   end
 
-  private
-  def format_decimal
+  def convert_to_decimal
     params[:form][:body].split("\n").each do |line|
         sign_char = line.include?("+") ? "+" : "-" 
         ra, dec = line.split(sign_char)
-        raH, raM,raS = ra.split(" ")
+        raH, raM, raS = ra.split(" ")
         decD, decM, decS = dec.split(" ")
-        @answers << to_decimal_degrees(raH.to_f, raM.to_f, raS.to_f, (sign_char + decD).to_f, decM.to_f, decS.to_f)
+        @answers << to_decimal(raH.to_f, raM.to_f, raS.to_f, (sign_char + decD).to_f, decM.to_f, decS.to_f)
     end
   end
 
-  def to_decimal_degrees(raH, raM, raS, decD, decM, decS)
+  def to_decimal(raH, raM, raS, decD, decM, decS)
       raDD = (raH) * 15 + (raM/4 + raS/240)
       decDD = (decD.abs + decM/60 + decS/3600 ) * (decD >= 0 ? 1.0 : -1.0)
     return raDD, decDD
@@ -34,14 +36,14 @@ class ConverterController < ApplicationController
 
   def to_sexigesimal(raDec, decDec)
       numbs = raDec.split('.')
-      raH = numbs[0].to_f/15.0
-      raM = numbs[1].to_f/60.0
-      raS = raM.to_s.split('.')[-1].to_f / 60.0
+      raH = numbs[0].to_f
+      raM = numbs[1].to_f / 60.0
+      raS = numbs[1].to_f / 3600.0
 
       numbs = decDec.split('.')
       decD = numbs[0].to_f
-      decM = numbs[1].to_f/60.0
-      decS = decM.to_s.split('.')[-1].to_f / 60.0
+      decM = numbs[1].to_f / 60.0
+      decS = numbs[1].to_f / 3600.0
       return "#{raH}:#{raM}:#{raS} #{decD > 0 ? "+" : "-"}#{decD}:#{decM}:#{decS}"
   end
 end
